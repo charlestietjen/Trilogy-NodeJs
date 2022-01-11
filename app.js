242717d6beff3bf4644a9ca4f66cdf8fa689d1e8
Charles T <tietjen.charles@gmail.com>
2022-01-11 17:11:16 -0500

    didnt save before merge, doh

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
 const printProfileData = (profileDataArr) => {
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-
-    console.log('==============');
-
     profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
